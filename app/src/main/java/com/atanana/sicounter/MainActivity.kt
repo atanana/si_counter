cbfa2aadd416f3cf941f5c847c9d155c7dc5b09a
atanana <tanana@email.ru>
2016-05-17 23:53:11 +0300

    menu init

--- app/src/main/java/com/atanana/sicounter/MainActivity.kt
+++ app/src/main/java/com/atanana/sicounter/MainActivity.kt
@@ -5,6 +5,9 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +19,26 @@ class MainActivity : AppCompatActivity() {
 
         val fab = findViewById(R.id.add_player) as FloatingActionButton?
         fab!!.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .show()
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mi_new -> {
+                Toast.makeText(this, "New player", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.mi_save -> {
+                Toast.makeText(this, "Save results", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
