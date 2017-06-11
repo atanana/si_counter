d804077dba28ac33732489ab65b859f01dd08f43
atanana <tanana@email.ru>
2017-06-11 14:18:25 +0300

    fix long names

--- app/src/main/java/com/atanana/sicounter/view/player_control/PlayerControl.kt
+++ app/src/main/java/com/atanana/sicounter/view/player_control/PlayerControl.kt
@@ -1,5 +1,6 @@
 package com.atanana.sicounter.view.player_control
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Typeface
 import android.util.AttributeSet
@@ -11,12 +12,14 @@ import com.atanana.sicounter.data.Score
 import com.atanana.sicounter.data.action.ScoreAction
 import com.atanana.sicounter.data.action.ScoreActionType.MINUS
 import com.atanana.sicounter.data.action.ScoreActionType.PLUS
+import com.atanana.sicounter.utils.UNSPECIFIED
 import com.atanana.sicounter.utils.dpToPx
 import com.atanana.sicounter.utils.pxToDp
 import com.atanana.sicounter.utils.screenSize
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 
+@Suppress("LeakingThis")
 open class PlayerControl(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
     private var playerName: TextView = TextView(context)
     private var playerScore: TextView = TextView(context)
@@ -32,6 +35,7 @@ open class PlayerControl(context: Context?, attrs: AttributeSet?) : LinearLayout
 
         playerName.textAlignment = View.TEXT_ALIGNMENT_CENTER
         playerName.typeface = Typeface.DEFAULT_BOLD
+        playerName.setSingleLine(true)
         addView(playerName)
 
         addScore.text = "+"
@@ -49,6 +53,7 @@ open class PlayerControl(context: Context?, attrs: AttributeSet?) : LinearLayout
         val width = right - left
         val height = bottom - top
         val slotHeight = height / 3
+        val slotPadding = (width - slotHeight) / 2
         val labelHeight = slotHeight / 2
 
         val labelTextSize = pxToDp(labelHeight, resources) * 2 / 3
@@ -57,20 +62,28 @@ open class PlayerControl(context: Context?, attrs: AttributeSet?) : LinearLayout
         playerName.textSize = labelTextSize
         playerName.layout(0, top, width, top + labelHeight)
 
-        addScore.layout(0, top + labelHeight, width, top + labelHeight + slotHeight)
+        addScore.layout(slotPadding, top + labelHeight, width - slotPadding, top + labelHeight + slotHeight)
         addScore.textSize = buttonTextSize
 
         playerScore.layout(0, top + labelHeight + slotHeight, width, top + labelHeight * 2 + slotHeight)
         playerScore.textSize = labelTextSize
 
-        subtractScore.layout(0, top + labelHeight * 2 + slotHeight, width, bottom)
+        subtractScore.layout(slotPadding, top + labelHeight * 2 + slotHeight, width - slotPadding, bottom)
         subtractScore.textSize = buttonTextSize
     }
 
+    @SuppressLint("DrawAllocation")
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val screenHeight = screenSize(context).height
         val height = Math.min((screenHeight / 2.5).toFloat(), dpToPx(300, resources))
-        val widthSpec = MeasureSpec.makeMeasureSpec((height / 3).toInt(), MeasureSpec.EXACTLY)
+        playerName.measure(UNSPECIFIED, UNSPECIFIED)
+        val slotWidth = (height / 3).toInt()
+        val width = Math.max(slotWidth, playerName.measuredWidth)
+
+        addScore.layoutParams = LinearLayout.LayoutParams(slotWidth, slotWidth)
+        subtractScore.layoutParams = LinearLayout.LayoutParams(slotWidth, slotWidth)
+
+        val widthSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY)
         val heightSpec = MeasureSpec.makeMeasureSpec(height.toInt(), MeasureSpec.EXACTLY)
         super.onMeasure(widthSpec, heightSpec)
     }
