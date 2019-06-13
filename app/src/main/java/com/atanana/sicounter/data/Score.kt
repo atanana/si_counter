efbcf20405b7b3a660da849482f4dd3629d824bd
Andrei Tanana <tanana@email.ru>
2019-06-13 23:18:40 +0300

    add parcelize

--- app/src/main/java/com/atanana/sicounter/data/Score.kt
+++ app/src/main/java/com/atanana/sicounter/data/Score.kt
@@ -1,31 +1,7 @@
 package com.atanana.sicounter.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class Score(val name: String, val score: Int) : Parcelable {
-    private constructor(parcel: Parcel) : this(parcel.readString(), parcel.readInt())
-
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        dest?.writeString(name)
-        dest?.writeInt(score)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object {
-        @Suppress("unused")
-        @JvmField
-        val CREATOR: Parcelable.Creator<Score> = object : Parcelable.Creator<Score> {
-            override fun createFromParcel(parcel: Parcel): Score {
-                return Score(parcel)
-            }
-
-            override fun newArray(size: Int): Array<Score?> {
-                return arrayOfNulls(size)
-            }
-        }
-    }
-}
+@Parcelize
+data class Score(val name: String, val score: Int) : Parcelable
