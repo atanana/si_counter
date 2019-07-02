a4fc796eb597e794ca2a00fe9cec25cc8b2d08c7
Andrei Tanana <andrei.tanana@deltatre.it>
2019-07-02 13:41:38 +0300

    refactoring

--- app/src/main/java/com/atanana/sicounter/model/log/LogNameModel.kt
+++ app/src/main/java/com/atanana/sicounter/model/log/LogNameModel.kt
@@ -10,26 +10,15 @@ const val EXTENSION = ".txt"
 class LogNameModel(private val newPlayersNameProvider: Observable<String>) {
     private var _filename = DEFAULT_FILE_NAME
 
-    private val filenameSubject: ReplaySubject<String> = ReplaySubject.createWithSize(1)
-    val filenameObservable: Observable<String> = filenameSubject
-
-    fun connect(): Disposable {
-        notifyFilenameChanged()
-
-        return newPlayersNameProvider.subscribe {
+    fun connect(): Disposable =
+        newPlayersNameProvider.subscribe {
             if (_filename == DEFAULT_FILE_NAME) {
                 _filename = it
             } else {
                 _filename += "-$it"
             }
-
-            notifyFilenameChanged()
         }
-    }
 
-    private fun notifyFilenameChanged() {
-        filenameSubject.onNext(fullFilename)
-    }
-
-    val fullFilename get() = _filename + EXTENSION
+    val fullFilename: String
+        get() = _filename + EXTENSION
 }
