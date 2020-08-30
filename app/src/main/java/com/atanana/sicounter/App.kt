01a2394fe6b7127bad66480573978ad5738a5d95
Andrei Tanana <tanana@email.ru>
2020-08-30 13:07:35 +0300

    update dependencies

--- app/src/main/java/com/atanana/sicounter/App.kt
+++ app/src/main/java/com/atanana/sicounter/App.kt
@@ -5,13 +5,14 @@ import com.atanana.sicounter.di.mainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR)
             androidContext(this@App)
             modules(mainModule)
         }
