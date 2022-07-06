850257e4ac6adb227e3c43a0016c1086a15b3111
Andrei Tanana <tanana@email.ru>
2022-07-06 14:31:12 +0300

    cleanup

--- app/src/main/java/com/atanana/sicounter/utils/MeasureSpecUtils.kt
+++ app/src/main/java/com/atanana/sicounter/utils/MeasureSpecUtils.kt
@@ -3,5 +3,3 @@ package com.atanana.sicounter.utils
 import android.view.View
 
 val UNSPECIFIED = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-
-fun exactly(size: Int) = View.MeasureSpec.makeMeasureSpec(size, View.MeasureSpec.EXACTLY)
