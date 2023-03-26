9c4231a75e2b784ef5d916b43539bc746100430c
Andrei Tanana <tanana.andrey@gmail.com>
2023-03-26 15:09:43 +0300

    update target api

--- app/src/main/java/com/atanana/sicounter/router/CreateLogFileContract.kt
+++ app/src/main/java/com/atanana/sicounter/router/CreateLogFileContract.kt
@@ -2,13 +2,12 @@ package com.atanana.sicounter.router
 
 import android.content.Context
 import android.content.Intent
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.CreateDocument
 
-class CreateLogFileContract : ActivityResultContracts.CreateDocument() {
+class CreateLogFileContract : CreateDocument("text/plain") {
 
     override fun createIntent(context: Context, input: String): Intent =
         super.createIntent(context, input).apply {
-            type = "text/plain"
             addCategory(Intent.CATEGORY_OPENABLE)
         }
 }
